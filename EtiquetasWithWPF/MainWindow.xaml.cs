81d8581eb68409f629da2d8135ec6a4f239f4ec5
a.obregon <a.obregon@HOME-WORK>
2023-07-31 13:25:57 -0700

    Add project files.

---
+++ EtiquetasWithWPF/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+
+namespace EtiquetasWithWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["conexionSQL"].ConnectionString;
+        private SqlConnection connection;
+        public MainWindow() => InitializeComponent();
+
+        public SqlConnection Connection { get => connection; set => connection = value; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Connection = new SqlConnection(connectionString: "Persist Security Info=False;Initial Catalog=SolmicroERP6_PruebasSub;Data Source=SERVIDOR\\SOLMICRO6;User ID=sa; pwd=Altai2021;Encrypt=True;TrustServerCertificate=True;");
+                Connection.Open();
+                string query = "Select * from tbMaestroCorreos";
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query,Connection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                dataGrid.ItemsSource = dataTable.DefaultView;
+               // Autocompletar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { Connection.Close(); }
+        }
+    }
+}
