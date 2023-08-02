5b74b25dc470add246e64e9d2c9492361fd403ec
Alejandro Obregón Rodríguez <a.obregon@favram.local>
2023-08-02 14:56:32 +0200

    Maquetando Rev 0.1

--- EtiquetasWithWPF/MVC/ViewModel/MainViewModel.cs
+++ EtiquetasWithWPF/MVC/ViewModel/MainViewModel.cs
@@ -5,6 +5,19 @@ namespace EtiquetasWithWPF.MVC.ViewModel
 {
     internal class MainViewModel:ObservableObject
     {
+        public EtiquetaModel EtiquetaVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChange();
+            }
+        }
+
         public MainViewModel() 
         { 
         
